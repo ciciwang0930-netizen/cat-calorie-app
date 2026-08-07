a325e89e66e1c5569ea37ed86d59c4557472fa8e
cecilia <cecilia@local>
2026-08-07 16:15:55 +0800

    v1.2: 手绘SVG猫脸头像（6毛色×4心情）替换emoji；重绘手绘小橘猫图标

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'neko-kcal-v2';
+const CACHE = 'neko-kcal-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-180.png', './icon-512.png', './'];
 
 self.addEventListener('install', e => {
