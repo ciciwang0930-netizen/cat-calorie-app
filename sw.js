85f2e55aa55da5421319ed949f4023d960d41fd9
cecilia <cecilia@local>
2026-08-07 16:41:03 +0800

    v1.3: 图标与头像换为多咪水彩画（自动主体裁剪+圆形粉圈）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'neko-kcal-v3';
-const ASSETS = ['./index.html', './manifest.json', './icon-180.png', './icon-512.png', './'];
+const CACHE = 'neko-kcal-v4';
+const ASSETS = ['./index.html', './manifest.json', './icon-180.png', './icon-512.png', './avatar.png', './'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
