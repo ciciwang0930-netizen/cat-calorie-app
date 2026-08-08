9586791b28709ac4b48e7da34cf674fd2383448d
cecilia <cecilia@local>
2026-08-08 11:42:44 +0800

    v14: 小鱼图标换为 🐈 emoji

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'neko-kcal-v13';
-const ASSETS = ['./index.html', './manifest.json', './icon-180.png', './icon-512.png', './avatar.png', './tab-home.png', './tab-foods.png', './fish.png', './'];
+const CACHE = 'neko-kcal-v14';
+const ASSETS = ['./index.html', './manifest.json', './icon-180.png', './icon-512.png', './avatar.png', './tab-home.png', './tab-foods.png', './'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
